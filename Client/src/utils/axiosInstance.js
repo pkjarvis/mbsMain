a7f6a527e2755ece4e6709d53fbe959042137acc
Dell <singh.pankaj@jarvis.consulting>
2025-07-02 11:28:03 +0530

    udpates

--- Client/src/utils/axiosInstance.js
+++ Client/src/utils/axiosInstance.js
@@ -4,7 +4,7 @@ import axios from "axios"
 
 const axiosInstance=axios.create({
     baseURL:"https://moviebookingsystem-ta6i.onrender.com",
-    timeout:5000,
+    timeout:15000,
     withCredentials:true,
     headers: {
     "Content-Type": "application/json",
