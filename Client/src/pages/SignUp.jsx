a5f8e9da2c74c554d7e77f1432dd359d7d2cb705
Dell <singh.pankaj@jarvis.consulting>
2025-07-02 11:06:46 +0530

    after deploy

--- Client/src/pages/SignUp.jsx
+++ Client/src/pages/SignUp.jsx
@@ -10,7 +10,7 @@ const SignUp = () => {
   const [password, setPassword] = useState("");
   const [error, setError] = useState(null);
 
-  const navigate = useNavigate("");
+  const navigate = useNavigate();
 
   const handleSignup = async (e) => {
     e.preventDefault();
