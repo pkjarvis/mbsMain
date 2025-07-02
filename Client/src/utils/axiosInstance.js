1827b36307c5f50009e052fe1c25663137cac0d0
Dell <singh.pankaj@jarvis.consulting>
2025-07-02 10:16:47 +0530

    deployed4

--- Client/src/utils/axiosInstance.js
+++ Client/src/utils/axiosInstance.js
@@ -3,7 +3,7 @@ import axios from "axios"
 
 
 const axiosInstance=axios.create({
-    baseURL:"",
+    baseURL:"https://cbb3-14-195-186-86.ngrok-free.app/",
     timeout:5000,
     withCredentials:true,
     body:JSON.stringify({}),
