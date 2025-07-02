c7b00f26369df2f01f709a2125544c090b680275
Dell <singh.pankaj@jarvis.consulting>
2025-07-02 12:16:53 +0530

    updates

--- internal/middlewares/isAuthorized.go
+++ internal/middlewares/isAuthorized.go
@@ -3,7 +3,7 @@
 package middlewares
 
 import (
-    "go-auth/utils"
+    // "go-auth/utils"
     "github.com/gin-gonic/gin"
     "fmt"
 )
@@ -11,39 +11,39 @@ import (
 func IsAuthorized() gin.HandlerFunc {
     return func(c *gin.Context) {
 
-        fmt.Println("Check in middleware isAuthorized");
+        // fmt.Println("Check in middleware isAuthorized");
 
-        cookie, err := c.Cookie("token")               
+        // cookie, err := c.Cookie("token")               
         
-        fmt.Println("cookie is:",cookie)
+        // fmt.Println("cookie is:",cookie)
 
-        fmt.Println("No error part 1")
-        if err != nil {
-            c.JSON(401, gin.H{"error": "unauthorized"})
-            c.Abort()
-            return
-        }
+        // fmt.Println("No error part 1")
+        // if err != nil {
+        //     c.JSON(401, gin.H{"error": "unauthorized"})
+        //     c.Abort()
+        //     return
+        // }
 
-        fmt.Println("IsAuthorized middleware is running")
+        // fmt.Println("IsAuthorized middleware is running")
 
-        fmt.Println("No error part 2")
+        // fmt.Println("No error part 2")
 
-        claims, err := utils.ParseToken(cookie)
+        // claims, err := utils.ParseToken(cookie)
 
-        fmt.Println("No error part 3")
+        // fmt.Println("No error part 3")
         
 
-        if err != nil {
-            c.JSON(401, gin.H{"error": "unauthorized"})
-            c.Abort()
-            return
-        }
+        // if err != nil {
+        //     c.JSON(401, gin.H{"error": "unauthorized"})
+        //     c.Abort()
+        //     return
+        // }
 
 
-        fmt.Println("No error part 4")
+        // fmt.Println("No error part 4")
 
-        c.Set("role", claims.Role)
-        c.Set("userId",claims.UserId)
+        // c.Set("role", claims.Role)
+        // c.Set("userId",claims.UserId)
         c.Next()
     }
 }
