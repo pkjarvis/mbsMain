ea08922c3cfc314d6e9f3ef2c2a4f05676e7a769
Dell <singh.pankaj@jarvis.consulting>
2025-07-03 10:41:59 +0530

    last and final update

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect } from "react";
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import Login from "./pages/Login";
 import Logout from "./pages/Logout";
