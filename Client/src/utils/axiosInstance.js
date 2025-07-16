061623c9fc4e7fbcd00c3de4fe54f4e7da3b23a1
Dell <singh.pankaj@jarvis.consulting>
2025-07-16 10:07:47 +0530

    added env in frontend

--- Client/src/utils/axiosInstance.js
+++ Client/src/utils/axiosInstance.js
@@ -3,7 +3,7 @@ import axios from "axios"
 
 
 const axiosInstance=axios.create({
-    baseURL:"http://localhost:8080",
+    baseURL:import.meta.env.VITE_API_URL,
     timeout:15000,
     withCredentials:true,
     headers: {
